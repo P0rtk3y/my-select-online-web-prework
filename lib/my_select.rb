52f8fc04430c9560b2de51440e6e840f779626ef
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-05 06:22:58 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,9 @@
 def my_select(collection)
  i = 0 
  new_collection = []
- while i < array.length
+ while i < collection.length
     if yield(array[i]) == true
     new_collection << array[i]
-  end
+ end
   new_collection 
 end
